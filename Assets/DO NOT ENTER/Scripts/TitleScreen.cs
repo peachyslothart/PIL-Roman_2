7ef6f59ed94f2dc7bb5397e41fb248033e187ee3
peachyslothart <nicolefolliott@gmail.com>
2024-02-11 21:43:57 -0600

    Project Moved

---
+++ Assets/DO NOT ENTER/Scripts/TitleScreen.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour {
+
+    public PanelFade panel;
+    bool musicOn = false;
+    public Text musicText;
+
+	// Use this for initialization
+	void Start () {
+        //panel.FadeIn();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        musicText.text = "MUSIC: " + (musicOn ? "ON" : "OFF");
+    }
+
+    public void StartGame()
+    {
+        GameManager.instance.AddTimer(2, GoToLevelSelect);
+        GameManager.instance.PlaySoundPlace();
+        panel.FadeOut();
+    }
+
+    public void GoToLevelSelect()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OptionsMenu()
+    {
+        GameManager.instance.AddTimer(2, GoToOptions);
+        GameManager.instance.PlaySoundPlace();
+        panel.FadeOut();
+    }
+
+    public void GoToOptions()
+    {
+        SceneManager.LoadScene("options");
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToggleMusic()
+    {
+        musicOn = !musicOn;
+
+        GameManager.instance.bgm = musicOn ? 3 : 0;
+    }
+}
